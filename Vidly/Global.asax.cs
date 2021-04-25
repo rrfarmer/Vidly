8ade9ca3ddfdf0612e13d8b1259003ef3e76783b
rrfarmer <ryanfarmer@mac.com>
2021-04-25 17:46:36 -0400

    Pre Exercise Section 6

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Vidly.App_Start;
 
 namespace Vidly
 {
@@ -13,6 +16,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
